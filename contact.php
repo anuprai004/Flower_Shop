9958006ea770e6b1f371e8ae1b0f96ff7756e7e5
nikeshadhikari9 <nickeshadhikari@gmail.com>
2025-04-17 23:00:58 +0545

    updated logic of quantity in cart

--- contact.php
+++ contact.php
@@ -6,11 +6,11 @@ session_start();
 
 $user_id = $_SESSION['user_id'];
 
-if(!isset($user_id)){
-   header('location:login.php');
+if (!isset($user_id)) {
+    header('location:login.php');
 };
 
-if(isset($_POST['send'])){
+if (isset($_POST['send'])) {
 
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -19,62 +19,72 @@ if(isset($_POST['send'])){
 
     $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
 
-    if(mysqli_num_rows($select_message) > 0){
+    if (mysqli_num_rows($select_message) > 0) {
         $message[] = 'message sent already!';
-    }else{
+    } else {
         mysqli_query($conn, "INSERT INTO `message`(user_id, name, email, number, message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('query failed');
         $message[] = 'message sent successfully!';
     }
-
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>contact</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom admin css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+    <!-- custom admin css file link  -->
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php @include 'header.php'; ?>
 
-<section class="heading">
-    <h3>contact us</h3>
-    <p> <a href="home.php">home</a> / contact </p>
-</section>
+    <?php @include 'header.php'; ?>
 
-<section class="contact">
+    <section class="heading">
+        <h3>contact us</h3>
+        <p> <a href="home.php">home</a> / contact </p>
+    </section>
 
-    <form action="" method="POST">
-        <h3>send us message!</h3>
-        <input type="text" name="name" placeholder="enter your name" class="box" required> 
-        <input type="email" name="email" placeholder="enter your email" class="box" required>
-        <input type="number" name="number" placeholder="enter your number" class="box" required>
-        <textarea name="message" class="box" placeholder="enter your message" required cols="30" rows="10"></textarea>
-        <input type="submit" value="send message" name="send" class="btn">
-    </form>
+    <section class="contact">
 
-</section>
+        <form action="" method="POST">
+            <h3>send us message!</h3>
+            <input type="text" name="name" placeholder="enter your name" class="box" required>
+            <input type="email" name="email" placeholder="enter your email" class="box" required>
+            <input type="number" name="number" placeholder="enter your phone number" class="box" id="number" required>
+            <textarea name="message" class="box" placeholder="enter your message" required cols="30" rows="10"></textarea>
+            <p id="error" style="color:red;font-size:large;"></p>
+            <input type="submit" value="send message" onclick="return contactSubmit()" name="send" class="btn">
+        </form>
 
+    </section>
 
+    <?php @include 'footer.php'; ?>
+    <script>
+        function contactSubmit() {
+            const error = document.getElementById("error");
+            const number = document.getElementById("number").value;
+            const numberStr = number.toString();
+            if (numberStr.length !== 10) {
+                error.textContent = "Phone number must be exactly 10 digits."
+                return false;
+            }
+            return true;
+        }
+    </script>
 
-
-
-
-<?php @include 'footer.php'; ?>
-
-<script src="js/script.js"></script>
+    <script src="js/script.js"></script>
 
 </body>
+
 </html>
--- shop.php
+++ shop.php
@@ -109,7 +109,7 @@ if (isset($_POST['add_to_cart'])) {
                         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
                         <img src="flowers/<?php echo $fetch_products['image']; ?>" alt="" class="image">
                         <div class="name"><?php echo $fetch_products['name']; ?></div>
-                        <input type="number" name="product_quantity" value="1" min="0" class="qty">
+                        <input type="number" name="product_quantity" value="1" min="1" class="qty">
                         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
                         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
                         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
